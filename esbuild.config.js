8b8bf99538a6012462fd3a2e97a1f735c9f14da3
Léderer András <andrew@file.glass>
2022-01-23 17:05:02 +0100

    chore(build):avoid bundling node deps

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,7 +1,9 @@
-require('esbuild').buildSync({
+const {nodeExternalsPlugin} =  require("esbuild-node-externals")
+require('esbuild').build({
     entryPoints: ['src/index.ts'],
     bundle: true,
     platform: 'node',
     target: ['node16'],
     outfile: 'dist/index.js',
+    plugins: [nodeExternalsPlugin()]
   })
